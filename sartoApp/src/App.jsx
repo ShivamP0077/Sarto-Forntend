38980d7227ae3cd27a7458f501c67210ea474d7c
Shivam parmar <134063660+ShivamP0077@users.noreply.github.com>
2025-05-03 17:07:15 +0530

    feat: initialize sarto application with React, Vite, and Tailwind CSS
    - Added package.json with dependencies for React, Vite, and Tailwind CSS.
    - Created postcss.config.js for PostCSS configuration.
    - Added Tailwind CSS directives in App.css.
    - Implemented main application component in App.jsx.
    - Included React logo SVG in assets.
    - Defined global styles in index.css.
    - Set up entry point in main.jsx for rendering the application.
    - Configured Tailwind CSS in tailwind.config.js.
    - Established Vite configuration in vite.config.js.

---
+++ sartoApp/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+         <p className='bg-red font-bold'>hello this is rontend</p>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
